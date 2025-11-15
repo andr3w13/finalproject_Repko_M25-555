de99b7d2ab463ca487d75308e6680676725194c5
andr3w13 <petrarka895@gmail.com>
2025-11-15 21:33:54 +0300

    Добавил загрузку данных из настроек

--- valutatrade_hub/core/constants.py
+++ valutatrade_hub/core/constants.py
@@ -1,11 +1,19 @@
 from pathlib import Path
-import json
+from valutatrade_hub.infra import settings
 
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
-DATA_PATH = PROJECT_ROOT / 'data'
-USERS_PATH = DATA_PATH / 'users.json'
-PORTFOLIOS_PATH = DATA_PATH / 'portfolios.json'
-RATES_PATH = DATA_PATH / 'rates.json'
+VALUTATRADE_HUB_PATH = Path(__file__).resolve().parents[1]
+
+config = settings.SettingsLoader(
+        VALUTATRADE_HUB_PATH / 'infra' / 'config.json'
+    )
+
+USERS_PATH = config.get('USERS_PATH')
+RATES_PATH = config.get('RATES_PATH')
+PORTFOLIOS_PATH = config.get('PORTFOLIOS_PATH')
+BASE_CURRENCY = config.get('BASE_CURRENCY')
+RATES_TTL_SECONDS = config.get('RATES_TTL_SECONDS')
+ACTIONS_LOG_PATH = config.get('ACTIONS_LOG_PATH')
+LOGS_RAW_FORMAT = config.get('LOG_FORMAT')
 
 CURRENT_SESSION = None
