724439c34776aed8b8ab298b632586a9bc0e205e
andr3w13 <petrarka895@gmail.com>
2025-11-16 21:15:43 +0300

    Добавил docstrings

--- valutatrade_hub/core/currencies.py
+++ valutatrade_hub/core/currencies.py
@@ -3,7 +3,11 @@ from valutatrade_hub.core import exceptions
 
 
 class Currency(ABC):
-    def __init__(self, name, code):
+    """
+    Абстрактный базовый класс для валюты.
+    Проверяет код (2–5 символов, без пробелов) и имя.
+    """
+    def __init__(self, name: str, code: str):
         if 2 <= len(code) <= 5 and ' ' not in code and name:
             self.name = name.upper()
             self.code = code
@@ -11,27 +15,30 @@ class Currency(ABC):
             print('Неверное заданы имя и/или код.')
     
     @abstractmethod
-    def get_display_info(self):
+    def get_display_info(self) -> None:
+        """Выводит информацию о валюте."""
         pass
 
 
 class CryptoCurrency(Currency):
+    """Криптовалюта с алгоритмом и капитализацией."""
     def __init__(self, name, code, algorithm, market_cap):
         super().__init__(name, code)
         self.algorithm = algorithm
         self.market_cap = market_cap
     
-    def get_display_info(self):
+    def get_display_info(self) -> None:
         print(f'[CRYPTO] {self.code} — {self.name} ' +
               f'(Algo: {self.algorithm}, MCAP: {self.market_cap:.2e})')
     
 
 class FiatCurrency(Currency):
-    def __init__(self, name, code, issuing_country):
+    """Фиатная валюта с эмитентом."""
+    def __init__(self, name: str, code: str, issuing_country: str):
         super().__init__(name, code)
         self.issuing_country = issuing_country
 
-    def get_display_info(self):
+    def get_display_info(self) -> None:
         print(f'[FIAT] {self.code} — {self.name} (Issuing: {self.issuing_country})')
 
 
@@ -44,7 +51,11 @@ CURRENCIES = {
 }
 
 
-def get_currency(code):
+def get_currency(code: str) -> Currency:
+    """
+    Возвращает объект валюты по коду.
+    Вызывает CurrencyNotFoundError, если валюта не найдена.
+    """
     code = code.upper()
     if code in CURRENCIES:
         return CURRENCIES[code]
