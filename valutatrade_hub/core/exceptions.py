dd64560625f1d51d2bd7dcca0bce8a8f3b386fd0
andr3w13 <petrarka895@gmail.com>
2025-11-16 21:16:18 +0300

    Добавил doctsrings

--- valutatrade_hub/core/exceptions.py
+++ valutatrade_hub/core/exceptions.py
@@ -1,15 +1,18 @@
 class CurrencyNotFoundError(Exception):
-    def __init__(self, code):
+    """Вызывается, если валюта с указанным кодом не найдена в системе."""
+    def __init__(self, code: str):
         super().__init__(f'Неизвестная валюта "{code}"')
 
 
 class InsufficientFundsError(Exception):
-    def __init__(self, available, code, required):
+    """Вызывается при попытке снять больше средств, чем есть на кошельке."""
+    def __init__(self, available: float, code: str, required: float):
         super().__init__('Недостаточно средств: ' +
                          f'доступно {available:,.2f} {code}, ' +
                          f'требуется {required:,.2f} {code}')
 
 
 class ApiRequestError(Exception):
-    def __init__(self, reason):
+    """Вызывается при ошибке обращения к внешнему API (сеть, статус, парсинг)."""
+    def __init__(self, reason: str):
         super().__init__(f'Ошибка при обращении к внешнему API: {reason}')
